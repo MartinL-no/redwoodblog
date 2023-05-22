7a6be5b28fe30841c16f89cb8f11915f1badbd4e
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-05-22 17:42:05 +0200

    Create article cell and add routing

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -9,9 +9,8 @@
 
 import { Router, Route, Set } from '@redwoodjs/router'
 
-import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
-
 import BlogLayout from 'src/layouts/BlogLayout'
+import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 
 const Routes = () => {
   return (
@@ -23,10 +22,11 @@ const Routes = () => {
         <Route path="/posts" page={PostPostsPage} name="posts" />
       </Set>
       <Set wrap={BlogLayout}>
+        <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
-        <Route notfound page={NotFoundPage} />
       </Set>
+      <Route notfound page={NotFoundPage} />
     </Router>
   )
 }
